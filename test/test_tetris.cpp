8dffad2a4c13e563c5dc50056f62755ba4b66b6d
Guilherme T. Faria <gui.faria.96@hotmail.com>
2025-04-17 23:11:17 -0300

    test: Testar `decodePiece` para a peça *I*

--- test/test_tetris.cpp
+++ test/test_tetris.cpp
@@ -7,7 +7,7 @@ void setUp(void){};
 void tearDown(void){};
 
 
-void test_decodePiece_I(){
+void test_decodePiece_I_rx0(){
   uint8_t piece[16] = {0};
   uint8_t expect_piece[16] = {
     0, 0, 0, 0,
@@ -17,12 +17,55 @@ void test_decodePiece_I(){
   };
   decodePiece(piece, piece_I[0]);
   TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+
 }
 
 
+void test_decodePiece_I_rx1(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 0, 1, 0,
+    0, 0, 1, 0,
+    0, 0, 1, 0,
+    0, 0, 1, 0
+  };
+
+  decodePiece(piece, piece_I[1]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+void test_decodePiece_I_rx2(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    1, 1, 1, 1,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_I[2]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+void test_decodePiece_I_rx3(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 0, 0
+  };
+  decodePiece(piece, piece_I[3]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
 int main(){
   UNITY_BEGIN();
-  RUN_TEST(test_decodePiece_I);
+  RUN_TEST(test_decodePiece_I_rx0);
+  RUN_TEST(test_decodePiece_I_rx1);
+  RUN_TEST(test_decodePiece_I_rx2);
+  RUN_TEST(test_decodePiece_I_rx3);
   return UNITY_END();
 
 }
