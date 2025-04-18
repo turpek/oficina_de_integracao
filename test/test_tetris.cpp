fa86d8c21fe122b458aa7c84d6ea216db197ad6a
Guilherme T. Faria <gui.faria.96@hotmail.com>
2025-04-18 01:18:50 -0300

    test: Testar `decodePiece` para as peças L, J, T, S, Z, O

--- test/test_tetris.cpp
+++ test/test_tetris.cpp
@@ -1,11 +1,23 @@
 #include "Unity/unity.h"
 #include "tetris/tetris.h"
+#include <iostream>
+using namespace std;
 
 
 void setUp(void){};
 
 void tearDown(void){};
 
+void show_piece(uint8_t *piece){
+
+  for (int i = 0; i < 16; ++i) {
+    cout << static_cast<int>(piece[i]);  // Tabulação para organizar melhor
+    if((i + 1) % 4 == 0) {
+      cout << endl;  // Quebra a linha a cada 4 elementos
+    }
+  }
+
+}
 
 void test_decodePiece_I_rx0(){
   uint8_t piece[16] = {0};
@@ -17,7 +29,6 @@ void test_decodePiece_I_rx0(){
   };
   decodePiece(piece, piece_I[0]);
   TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
-
 }
 
 
@@ -60,12 +71,350 @@ void test_decodePiece_I_rx3(){
   TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
 }
 
+
+void test_decodePiece_L_rx0(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 0, 1, 0,
+    1, 1, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  };
+
+  decodePiece(piece, piece_L[0]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+void test_decodePiece_L_rx1(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 1, 0,
+    0, 0, 0, 0
+  };
+
+  decodePiece(piece, piece_L[1]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+void test_decodePiece_L_rx2(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 0, 0, 0,
+    1, 1, 1, 0,
+    1, 0, 0, 0,
+    0, 0, 0, 0
+  };
+
+  decodePiece(piece, piece_L[2]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+void test_decodePiece_L_rx3(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    1, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0
+  };
+
+  decodePiece(piece, piece_L[3]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+void test_decodePiece_J_rx0(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    1, 0, 0, 0,
+    1, 1, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_J[0]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+
+}
+
+
+void test_decodePiece_J_rx1(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 1, 1, 0,
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_J[1]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+
+}
+
+
+void test_decodePiece_J_rx2(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 0, 0, 0,
+    1, 1, 1, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_J[2]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+void test_decodePiece_J_rx3(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_J[3]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+void test_decodePiece_T_rx0(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 1, 0, 0,
+    1, 1, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_T[0]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+void test_decodePiece_T_rx1(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 1, 0, 0,
+    0, 1, 1, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_T[1]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+void test_decodePiece_T_rx2(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 0, 0, 0,
+    1, 1, 1, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_T[2]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+
+void test_decodePiece_T_rx3(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 1, 0, 0,
+    1, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_T[3]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+void test_decodePiece_S_rx0(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 1, 1, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_S[0]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+void test_decodePiece_S_rx1(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 1, 0, 0,
+    0, 1, 1, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_S[1]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+void test_decodePiece_S_rx2(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 0, 0, 0,
+    0, 1, 1, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_S[2]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+
+}
+void test_decodePiece_S_rx3(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    1, 0, 0, 0,
+    1, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_S[3]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+void test_decodePiece_Z_rx0(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    1, 1, 0, 0,
+    0, 1, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_Z[0]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+void test_decodePiece_Z_rx1(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 0, 1, 0,
+    0, 1, 1, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_Z[1]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+void test_decodePiece_Z_rx2(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 0, 0, 0,
+    1, 1, 0, 0,
+    0, 1, 1, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_Z[2]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+void test_decodePiece_Z_rx3(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    0, 1, 0, 0,
+    1, 1, 0, 0,
+    1, 0, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_Z[3]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+void test_decodePiece_O_rx0(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    1, 1, 0, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_O[0]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+
+void test_decodePiece_O_rx1(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    1, 1, 0, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_O[1]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+void test_decodePiece_O_rx2(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    1, 1, 0, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_O[2]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
+void test_decodePiece_O_rx3(){
+  uint8_t piece[16] = {0};
+  uint8_t expect_piece[16] = {
+    1, 1, 0, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0
+  };
+  decodePiece(piece, piece_O[3]);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
+}
+
 int main(){
   UNITY_BEGIN();
   RUN_TEST(test_decodePiece_I_rx0);
   RUN_TEST(test_decodePiece_I_rx1);
   RUN_TEST(test_decodePiece_I_rx2);
   RUN_TEST(test_decodePiece_I_rx3);
+
+  RUN_TEST(test_decodePiece_L_rx0);
+  RUN_TEST(test_decodePiece_L_rx1);
+  RUN_TEST(test_decodePiece_L_rx2);
+  RUN_TEST(test_decodePiece_L_rx3);
+
+  RUN_TEST(test_decodePiece_J_rx0);
+  RUN_TEST(test_decodePiece_J_rx1);
+  RUN_TEST(test_decodePiece_J_rx2);
+  RUN_TEST(test_decodePiece_J_rx3);
+
+  RUN_TEST(test_decodePiece_T_rx0);
+  RUN_TEST(test_decodePiece_T_rx1);
+  RUN_TEST(test_decodePiece_T_rx2);
+  RUN_TEST(test_decodePiece_T_rx3);
+
+  RUN_TEST(test_decodePiece_S_rx0);
+  RUN_TEST(test_decodePiece_S_rx1);
+  RUN_TEST(test_decodePiece_S_rx2);
+  RUN_TEST(test_decodePiece_S_rx3);
+
+  RUN_TEST(test_decodePiece_Z_rx0);
+  RUN_TEST(test_decodePiece_Z_rx1);
+  RUN_TEST(test_decodePiece_Z_rx2);
+  RUN_TEST(test_decodePiece_Z_rx3);
+
+  RUN_TEST(test_decodePiece_O_rx0);
+  RUN_TEST(test_decodePiece_O_rx1);
+  RUN_TEST(test_decodePiece_O_rx2);
+  RUN_TEST(test_decodePiece_O_rx3);
   return UNITY_END();
 
 }
--- tetris/tetris.cpp
+++ tetris/tetris.cpp
@@ -3,17 +3,37 @@
 #define NUM_PIECE_TYPES     7
 #define NUM_ROTATION        4
 
-/*                      Peça I
+/*                            Peça I
  *
- *    modo normal Rotacionado 1x Rotacionado 2x Rotacionado 3x
+ *   Rotacional 0x Rotacionado 1x Rotacionado 2x Rotacionado 3x
  *       0000         0010           0000           0100
  *       1111         0010           0000           0100
  *       0000         0010           1111           0100
  *       0000         0010           0000           0100
- * Hex  0x0F00
+ * Hex  0x0F00       0x2222          0x00F0        0x4444
+ *
+ *                            Peça L
+ *
+ *    Rotacionado 0x Rotacionado 1x Rotacionado 2x Rotacionado 3x
+ *        0010           0100           0000           1100          
+ *        1110           0100           1110           0100          
+ *        0000           0110           1000           0100          
+ *        0000           0000           0000           0000          
+ * Hex   0x2E00         0x4460         0x0E80         0xC440
+ *
+ *                            Peça J
+ *
+ *  Rotacionado 0x Rotacionado 1x Rotacionado 2x Rotacionado 3x
+ *      1000          0110           0000           0100
+ *      1110          0100           1110           0100
+ *      0000          0100           0010           1100
+ *      0000          0000           0000           0000
+ * Hex 0x8E00        0x6440         0x1CA0         0xFCC0
  */
 
 uint16_t piece_I[NUM_ROTATION] = {0x0F00, 0x2222, 0x00F0, 0x4444};
+uint16_t piece_L[NUM_ROTATION] = {0x2E00, 0x4460, 0x0E80, 0xC440};
+uint16_t piece_J[NUM_ROTATION] = {0x8E00, 0x6440, 0x0000, 0x0000};
 
 
 void decodePiece(uint8_t *piece, const uint16_t bitmask){
