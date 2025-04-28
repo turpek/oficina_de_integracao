5b9267da2a17ca67b46b2f197b943a3c65e932e4
Guilherme T. Faria <gui.faria.96@hotmail.com>
2025-04-28 00:46:40 -0300

    test: Adicionar teste para a função `add_piece_to_grid` com a peça `S`

--- test/test_tetris.cpp
+++ test/test_tetris.cpp
@@ -1,10 +1,20 @@
 #include "Unity/unity.h"
 #include "tetris/tetris.h"
+#include "test/test_utils.h"
 #include <iostream>
 using namespace std;
 
+#define GRID_W 8
+#define GRID_H 16
+#define LED_COUNT (GRID_W * GRID_H)
 
-void setUp(void){};
+void setUp(void){
+  clear_grid();
+  piece_id = 0;
+  piece_rotation = 0;
+  piece_x = 0;
+  piece_y = 0;
+};
 
 void tearDown(void){};
 
@@ -14,6 +24,20 @@ void show_piece(uint8_t *piece){
     cout << static_cast<int>(piece[i]);  // Tabulação para organizar melhor
     if((i + 1) % 4 == 0) {
       cout << endl;  // Quebra a linha a cada 4 elementos
+    }
+  }
+
+}
+
+void show_grid(uint32_t *grid){
+
+  for (int i = 0; i < LED_COUNT; ++i) {
+    int val = grid[i];
+    if(val != 0)
+      val = 1;
+    cout << val;  // Tabulação para organizar melhor
+    if((i + 1) % 8 == 0) {
+      cout << endl;  // Quebra a linha a cada 8 elementos
     }
   }
 
@@ -379,6 +403,45 @@ void test_decodePiece_O_rx3(){
   TEST_ASSERT_EQUAL_INT8_ARRAY(expect_piece, piece, 16);
 }
 
+void test_add_piece_to_grid_Piece_S_rx0(){
+  uint32_t expect_grid[LED_COUNT] = {0};
+  piece_id = 0;
+  piece_rotation = 0;
+  add_piece_to_grid();
+  transform_grid(expect_grid, map_piece_S_rx0, piece_colors[piece_id], LED_COUNT);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_grid, grid, LED_COUNT);
+}
+
+
+void test_add_piece_to_grid_Piece_S_rx1(){
+  uint32_t expect_grid[LED_COUNT] = {0};
+  piece_id = 0;
+  piece_rotation = 1;
+  add_piece_to_grid();
+  transform_grid(expect_grid, map_piece_S_rx1, piece_colors[piece_id], LED_COUNT);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_grid, grid, LED_COUNT);
+}
+
+
+void test_add_piece_to_grid_Piece_S_rx2(){
+  uint32_t expect_grid[LED_COUNT] = {0};
+  piece_id = 0;
+  piece_rotation = 2;
+  add_piece_to_grid();
+  transform_grid(expect_grid, map_piece_S_rx2, piece_colors[piece_id], LED_COUNT);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_grid, grid, LED_COUNT);
+}
+
+void test_add_piece_to_grid_Piece_S_rx3(){
+  uint32_t expect_grid[LED_COUNT] = {0};
+  piece_id = 0;
+  piece_rotation = 3;
+  add_piece_to_grid();
+  transform_grid(expect_grid, map_piece_S_rx3, piece_colors[piece_id], LED_COUNT);
+  TEST_ASSERT_EQUAL_INT8_ARRAY(expect_grid, grid, LED_COUNT);
+}
+
+
 int main(){
   UNITY_BEGIN();
   RUN_TEST(test_decodePiece_I_rx0);
@@ -415,6 +478,12 @@ int main(){
   RUN_TEST(test_decodePiece_O_rx1);
   RUN_TEST(test_decodePiece_O_rx2);
   RUN_TEST(test_decodePiece_O_rx3);
+
+
+  RUN_TEST(test_add_piece_to_grid_Piece_S_rx0);
+  RUN_TEST(test_add_piece_to_grid_Piece_S_rx1);
+  RUN_TEST(test_add_piece_to_grid_Piece_S_rx2);
+  RUN_TEST(test_add_piece_to_grid_Piece_S_rx3);
   return UNITY_END();
 
 }
