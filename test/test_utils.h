8c7b898230c3d9fff983fc7631dc4089175e98e8
Guilherme T. Faria <gui.faria.96@hotmail.com>
2025-05-13 02:50:27 -0300

    test: Testar `can_rotate` com os novos KICKS

--- test/test_utils.h
+++ test/test_utils.h
@@ -24,8 +24,7 @@ extern uint32_t map_piece_S_rx0_movX6Y14[];
 extern uint32_t map_piece_S_rx0_movX5Y15[];
 extern uint32_t map_piece_S_rx0_movX6Y15[];
 
-extern uint32_t map_rotation_L_OR[];
-extern uint32_t map_rotation_L_R2[];
-extern uint32_t map_rotation_shift_2x[];
+extern uint32_t map_rotation_L[];
+extern uint32_t map_rotation_I[];
 
 #endif
