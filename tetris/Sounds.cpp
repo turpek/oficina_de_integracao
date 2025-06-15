99cd46d774c117d2ddf165abd68a0ae8837d7611
Guilherme T. Faria <gui.faria.96@hotmail.com>
2025-06-14 22:24:05 -0300

    feat: Adicionar a trilha sonora

---
+++ tetris/Sounds.cpp
@@ -0,0 +1,102 @@
+#include "Arduino.h"
+#include "Sounds.h"
+#include "Chrono.h"
+
+ /*
+  Tetris theme - (Korobeiniki)
+  Connect a piezo buzzer or speaker to pin 11 or select a new pin.
+  More songs available at https://github.com/robsoncouto/arduino-songs
+
+                                              Robson Couto, 2019
+*/
+
+Chrono songTimer;
+
+// change this to make the song slower or faster
+int tempo=120;
+#define TEMPO 120
+#define BUZZER_TETRIS 2
+// change this to whichever pin you want to use
+int buzzer = 11;
+
+
+// notes of the moledy followed by the duration.
+// a 4 means a quarter note, 8 an eighteenth , 16 sixteenth, so on
+// !!negative numbers are used to represent dotted notes,
+// so -4 means a dotted quarter note, that is, a quarter plus an eighteenth!!
+int melody[] = {
+
+  //Based on the arrangement at https://www.flutetunes.com/tunes.php?id=192
+
+  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
+  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, REST,4,
+
+  REST,8, NOTE_D5, 4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
+  NOTE_E5, -4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, REST, 4,
+
+  NOTE_E5,2, NOTE_C5,2,
+  NOTE_D5,2, NOTE_B4,2,
+  NOTE_C5,2, NOTE_A4,2,
+  NOTE_B4,1,
+
+  NOTE_E5,2, NOTE_C5,2,
+  NOTE_D5,2, NOTE_B4,2,
+  NOTE_C5,4, NOTE_E5,4, NOTE_A5,2,
+  NOTE_GS5,1,
+
+  NOTE_E5, 4,  NOTE_B4,8,  NOTE_C5,8,  NOTE_D5,4,  NOTE_C5,8,  NOTE_B4,8,
+  NOTE_A4, 4,  NOTE_A4,8,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  NOTE_B4, -4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  NOTE_C5, 4,  NOTE_A4,4,  NOTE_A4,4, REST,4,
+
+  REST,8, NOTE_D5, 4,  NOTE_F5,8,  NOTE_A5,4,  NOTE_G5,8,  NOTE_F5,8,
+  REST,8, NOTE_E5, 4,  NOTE_C5,8,  NOTE_E5,4,  NOTE_D5,8,  NOTE_C5,8,
+  REST,8, NOTE_B4, 4,  NOTE_C5,8,  NOTE_D5,4,  NOTE_E5,4,
+  REST,8, NOTE_C5, 4,  NOTE_A4,8,  NOTE_A4,4, REST, 4,
+
+};
+
+// sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
+// there are two values per note (pitch and duration), so for each note there are four bytes
+int notes=sizeof(melody)/sizeof(melody[0])/2;
+
+// this calculates the duration of a whole note in ms (60s/tempo)*4 beats
+int wholenote = (60000 * 4) / TEMPO;
+
+int divider = 0, noteDuration = 0;
+
+void tetrisSong() {
+  static int thisNote = 0;
+  // iterate over the notes of the melody.
+  // Remember, the array is twice the number of notes (notes + durations)
+  if(songTimer.hasPassed(noteDuration)){
+    if(thisNote < notes * 2) {
+      // stop the waveform generation before the next note.
+      noTone(BUZZER_TETRIS);
+
+      // calculates the duration of each note
+      divider = melody[thisNote + 1];
+      if (divider > 0) {
+        // regular note, just proceed
+        noteDuration = (wholenote) / divider;
+      } else if (divider < 0) {
+        // dotted notes are represented with negative durations!!
+        noteDuration = (wholenote) / abs(divider);
+        noteDuration *= 1.5; // increases the duration in half for dotted notes
+      }
+
+      // we only play the note for 90% of the duration, leaving 10% as a pause
+      tone(buzzer, melody[thisNote], noteDuration*0.9);
+
+      songTimer.restart();
+      thisNote = thisNote + 2;
+    }
+    else{
+      thisNote = 0;
+    }
+  }
+}
